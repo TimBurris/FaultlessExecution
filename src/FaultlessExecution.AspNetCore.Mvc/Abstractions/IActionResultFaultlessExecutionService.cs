2034c8c21db390659fde1a4c5250dcc2464466a7
Tim Burris <tim.burris@chalklogi.com>
2020-04-28 16:24:09 -0400

    porting the Mvc helper from local to use to the shared repo

---
+++ src/FaultlessExecution.AspNetCore.Mvc/Abstractions/IActionResultFaultlessExecutionService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaultlessExecution.AspNetCore.Mvc.Abstractions
+{
+    public interface IActionResultFaultlessExecutionService
+    {
+
+        /// <summary>
+        /// Invokes <paramref name="code"/> handling errors and returning an <see cref="IActionResult"/>
+        /// </summary>
+        /// <typeparam name="T">The type to be returned by <paramref name="code"/> which will be included in <see cref="OkObjectResult"/> </typeparam>
+        /// <param name="code">The code to invoke</param>
+        /// <returns>If successful, an <see cref="OkObjectResult"/> with the <typeparamref name="T"/> value as the content
+        /// if unsuccessful, a <see cref="BadRequestObjectResult"/> with content provided by <see cref="ActionResultFaultlessExecutionServiceConfiguration.BadRequestObjectBuilder"/>
+        /// </returns>
+        Task<IActionResult> TryExecuteAsync<T>(Func<Task<T>> code);
+
+        /// <summary>
+        /// Invokes <paramref name="code"/> handling errors and returning an <see cref="IActionResult"/>
+        /// </summary>
+        /// <param name="code">The code to invoke</param>
+        /// <returns>If successful, a <see cref="NoContentResult"/> 
+        /// if unsuccessful, a <see cref="BadRequestObjectResult"/> with content provided by <see cref="ActionResultFaultlessExecutionServiceConfiguration.BadRequestObjectBuilder"/>
+        /// </returns>
+        Task<IActionResult> TryExecuteAsync(Func<Task> code);
+
+        /// <summary>
+        /// Peforms a <see cref="TryExecuteAsync{T}(Func{T})"/> on <paramref name="code"/> by wrapping it inside of a Task.Run
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="code">The code to be executed</param>
+        /// <returns>If successful, an <see cref="OkObjectResult"/> with the <typeparamref name="T"/> value as the content
+        /// if unsuccessful, a <see cref="BadRequestObjectResult"/> with content provided by <see cref="ActionResultFaultlessExecutionServiceConfiguration.BadRequestObjectBuilder"/>
+        /// </returns>
+        Task<IActionResult> TryExecuteSyncAsAsync<T>(Func<T> code);
+
+        /// <summary>
+        /// Peforms a <see cref="TryExecuteAsync(Func{Task})"/> on <paramref name="code"/> by wrapping it inside of a Task.Run
+        /// </summary>
+        /// <param name="code">The code to be executed</param>
+        /// <returns>If successful, a <see cref="NoContentResult"/> 
+        /// if unsuccessful, a <see cref="BadRequestObjectResult"/> with content provided by <see cref="ActionResultFaultlessExecutionServiceConfiguration.BadRequestObjectBuilder"/>
+        /// </returns>
+        Task<IActionResult> TryExecuteSyncAsAsync(Action code);
+
+        /// <summary>
+        /// Optional Configuration Options for this instance.  If not specified a <see cref="ActionResultFaultlessExecutionServiceConfiguration.DefaultConfiguration"/> will be used
+        /// </summary>
+        ActionResultFaultlessExecutionServiceConfiguration Configuration { get; set; }
+    }
+}
---
+++ src/FaultlessExecution.AspNetCore.Mvc/ActionResultFaultlessExecutionService.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace FaultlessExecution.AspNetCore.Mvc
+{
+    public class ActionResultFaultlessExecutionService : Abstractions.IActionResultFaultlessExecutionService
+    {
+        private readonly FaultlessExecution.Abstractions.IFaultlessExecutionService _faultlessExecutionService;
+        private readonly Microsoft.Extensions.Logging.ILogger<ActionResultFaultlessExecutionService> _logger;
+
+        public ActionResultFaultlessExecutionService(FaultlessExecution.Abstractions.IFaultlessExecutionService faultlessExecutionService,
+            Microsoft.Extensions.Logging.ILogger<ActionResultFaultlessExecutionService> logger)
+        {
+            _faultlessExecutionService = faultlessExecutionService;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> TryExecuteAsync<T>(Func<Task<T>> code)
+        {
+            var result = await _faultlessExecutionService.TryExecuteAsync(code);
+            if (result.WasSuccessful)
+            {
+                return new OkObjectResult(result.ReturnValue);
+            }
+            else
+            {
+                _logger.LogError(result.Exception, "Exception caught; BadRequest with FailModel will be sent to client");
+                return new BadRequestObjectResult(this.BuildBadRequestObject(result));
+            }
+        }
+
+        public async Task<IActionResult> TryExecuteSyncAsAsync<T>(Func<T> code)
+        {
+            Func<Task<T>> x = () => Task.Run<T>(code);
+            return await this.TryExecuteAsync(x);
+        }
+
+
+        public async Task<IActionResult> TryExecuteAsync(Func<Task> code)
+        {
+            var result = await _faultlessExecutionService.TryExecuteAsync(code);
+            if (result.WasSuccessful)
+            {
+                return new NoContentResult();
+            }
+            else
+            {
+                _logger.LogError(result.Exception, "Exception caught; BadRequest with FailModel will be sent to client");
+                return new BadRequestObjectResult(this.BuildBadRequestObject(result));
+            }
+        }
+
+
+        public async Task<IActionResult> TryExecuteSyncAsAsync(Action code)
+        {
+            Func<Task> x = () => Task.Run(code);
+            return await this.TryExecuteAsync(x);
+        }
+
+        public ActionResultFaultlessExecutionServiceConfiguration Configuration { get; set; }
+
+        protected object BuildBadRequestObject(FaultlessExecution.Abstractions.ExecutionResult result)
+        {
+            var config = (this.Configuration ?? ActionResultFaultlessExecutionServiceConfiguration.DefaultConfiguration);
+
+
+            return config?.BadRequestObjectBuilder?.Invoke(result);
+        }
+    }
+
+    //T O D O: find away to make this class name longer
+    public class ActionResultFaultlessExecutionServiceConfiguration : ICloneable
+    {
+        #region Static Default Config
+        private static ActionResultFaultlessExecutionServiceConfiguration _defaultConfiguration;
+
+        /// <summary>
+        /// gets or sets the configuration that will be used by all instances of <see cref="Abstractions.IActionResultFaultlessExecutionService"/> who don't have an explicit Configuration assigned
+        /// </summary>
+        public static ActionResultFaultlessExecutionServiceConfiguration DefaultConfiguration
+        {
+            get
+            {
+                if (_defaultConfiguration == null)
+                {
+                    _defaultConfiguration = new ActionResultFaultlessExecutionServiceConfiguration()
+                    {
+                        BadRequestObjectBuilder = (result) => result?.Exception?.Message, //default is to just use the errormessage
+                    };
+                }
+                return _defaultConfiguration;
+            }
+        }
+
+        #endregion
+
+        private Func<FaultlessExecution.Abstractions.ExecutionResult, object> _badRequestObjectBuilder;
+
+        public Func<FaultlessExecution.Abstractions.ExecutionResult, object> BadRequestObjectBuilder
+        {
+            get { return _badRequestObjectBuilder; }
+            set { _badRequestObjectBuilder = value; }
+        }
+
+        public ActionResultFaultlessExecutionServiceConfiguration Clone()
+        {
+            return new ActionResultFaultlessExecutionServiceConfiguration()
+            {
+                BadRequestObjectBuilder = this.BadRequestObjectBuilder,
+            };
+        }
+
+        object ICloneable.Clone() { return Clone(); }
+    }
+}
