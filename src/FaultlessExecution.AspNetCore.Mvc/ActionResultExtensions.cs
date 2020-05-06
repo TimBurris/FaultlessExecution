b7763f580a7f0a979afd312195b75f7b7ca627d4
Tim Burris <tim.burris@chalklogi.com>
2020-05-06 07:51:14 -0400

    added extentions methods for MVC Null then NotFound Result

---
+++ src/FaultlessExecution.AspNetCore.Mvc/ActionResultExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultlessExecution.Extensions
+{
+    public static class ActionResultExtensions
+    {
+        public static async Task<IActionResult> OnOkNullReturnNotFound(this Task<IActionResult> t)
+        {
+            var result = await t;
+
+            return OnOkNullReturnNotFound(result);
+        }
+
+        public static IActionResult OnOkNullReturnNotFound(this IActionResult actionResult)
+        {
+            var okResult = actionResult as OkObjectResult;
+
+            if (okResult == null)
+                return actionResult;
+            else if (okResult.Value == null)
+                return new NotFoundResult();
+            else
+                return actionResult;
+        }
+    }
+}
